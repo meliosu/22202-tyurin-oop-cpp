5e8e504b38f10e89bd5cf80d626192d4ef445605
Alexander <a.tyurin@g.nsu.ru>
2023-09-20 01:23:28 +0700

    added Parser class implementation

--- task-0/Parser.cpp
+++ task-0/Parser.cpp
@@ -3,3 +3,27 @@
 //
 
 #include "Parser.h"
+
+string Parser::toLower(string word) {
+    for (int i = 0; i < word.size(); i++) {
+        word[i] = std::tolower(word[i]);
+    }
+
+    return word;
+}
+
+std::list<string> Parser::parse(string line) {
+    std::list<string> words = {};
+
+    std::regex r("[[:alnum:]']+");
+    std::sregex_iterator i(line.begin(), line.end(), r);
+    std::sregex_iterator end;
+    while (i != end) {
+        string word = i->str();
+        toLower(word);
+        words.push_front(toLower(word));
+        i++;
+    }
+
+    return words;
+}
--- task-0/Parser.h
+++ task-0/Parser.h
@@ -5,9 +5,19 @@
 #ifndef TASK_0_PARSER_H
 #define TASK_0_PARSER_H
 
+#include <list>
+#include <string>
+#include <regex>
+
+using std::string;
 
 class Parser {
-
+private:
+    // converts all letters in a string to lowercase
+    string toLower(string word);
+public:
+    // converts a string to a list of words
+    std::list<string> parse(string line);
 };
 
 
