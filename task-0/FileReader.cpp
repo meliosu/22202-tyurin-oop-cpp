3b1e44399be899bc857b12df8d81ef31780a707d
Alexander <a.tyurin@g.nsu.ru>
2023-09-20 00:13:52 +0700

    added FileReader class implementation

--- task-0/FileReader.cpp
+++ task-0/FileReader.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "FileReader.h"
+
+FileReader::FileReader(std::string filename) {
+    this->filename = filename;
+}
+
+void FileReader::open() {
+    file.open(filename);
+}
+
+void FileReader::close() {
+    file.close();
+}
+
+bool FileReader::hasNext() {
+    return !file.eof();
+}
+
+string FileReader::getNext() {
+    if (!hasNext()) {
+        return "";
+    }
+
+    std::string next_line;
+    std::getline(file, next_line);
+    return next_line;
+}
+
+void FileReader::reset() {
+    file.seekg(std::ios_base::beg);
+}
--- task-0/FileReader.h
+++ task-0/FileReader.h
@@ -5,9 +5,29 @@
 #ifndef TASK_0_FILEREADER_H
 #define TASK_0_FILEREADER_H
 
+#include <string>
+#include <fstream>
+#include <iostream>
+
+using std::string;
+using std::ifstream;
 
 class FileReader {
+private:
+    string filename;
+    ifstream file;
+public:
+    FileReader(string filename);
 
+    void open();
+
+    void close();
+
+    string getNext();
+
+    bool hasNext();
+
+    void reset();
 };
 
 
