51c663b738258ffa968c9524b900eb2832c1acd5
Alexander <a.tyurin@g.nsu.ru>
2023-09-20 18:38:49 +0700

    added main program *everything works*

--- task-0/main.cpp
+++ task-0/main.cpp
@@ -1,11 +1,39 @@
-#include <iostream>
 #include "FileReader.h"
 #include "Parser.h"
 #include "FrequencyList.h"
 #include "FileWriter.h"
 
+#include <string>
+#include <list>
 
-int main() {
+using std::string;
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        exit(1);
+    }
+
+    FileReader fileReader(argv[1]);
+    fileReader.open();
+
+    Parser parser;
+    FrequencyList frequencyList;
+
+    while (fileReader.hasNext()) {
+        string line = fileReader.getNext();
+        std::list<string> words = parser.parse(line);
+        for (auto word : words) {
+            frequencyList.addWord(word);
+        }
+    }
+
+    FileWriter fileWriter(argv[2]);
+    fileWriter.open();
+
+    fileWriter.writeFrequencyList(frequencyList);
+
+    fileReader.close();
+    fileWriter.close();
 
     return 0;
 }
