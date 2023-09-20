47eddb4a8a61ca05e5930a56c02a9deee2849b28
Alexander <a.tyurin@g.nsu.ru>
2023-09-20 13:50:24 +0700

    added FrequencyList class implementation

--- task-0/FrequencyList.cpp
+++ task-0/FrequencyList.cpp
@@ -3,3 +3,30 @@
 //
 
 #include "FrequencyList.h"
+
+FrequencyList::FrequencyList() {
+    frequencyMap = {};
+    wordCount = 0;
+}
+
+void FrequencyList::addWord(string word) {
+    frequencyMap[word]++;
+    wordCount++;
+}
+
+int FrequencyList::getWordCount() {
+    return wordCount;
+}
+
+std::list<std::pair<string, int>> FrequencyList::getSortedList() {
+    std::list<std::pair<string, int>> sortedList = {};
+    for (auto pair : frequencyMap) {
+        sortedList.push_front(std::pair<string, int>(pair.first, pair.second));
+    }
+
+    sortedList.sort([](std::pair<string, int> w1, std::pair<string, int> w2){
+        return w1.second > w2.second;
+    });
+
+    return sortedList;
+}
--- task-0/FrequencyList.h
+++ task-0/FrequencyList.h
@@ -5,9 +5,24 @@
 #ifndef TASK_0_FREQUENCYLIST_H
 #define TASK_0_FREQUENCYLIST_H
 
+#include <unordered_map>
+#include <list>
+#include <string>
+
+using std::string;
 
 class FrequencyList {
+private:
+    std::unordered_map<string, int>  frequencyMap;
+    int wordCount;
+public:
+    FrequencyList();
 
+    void addWord(string word);
+
+    int getWordCount();
+
+    std::list<std::pair<string, int>> getSortedList();
 };
 
 
