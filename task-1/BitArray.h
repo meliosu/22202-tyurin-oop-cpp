60700ee75243f498b26b187e2b1700feb8fb0a85
Alexander <a.tyurin@g.nsu.ru>
2023-10-13 20:36:15 +0700

    added BitArray public members

--- task-1/BitArray.h
+++ task-1/BitArray.h
@@ -5,10 +5,71 @@
 #ifndef TASK_1_BITARRAY_H
 #define TASK_1_BITARRAY_H
 
+#include <string>
 
 class BitArray {
+public:
+    BitArray();
+    ~BitArray();
 
+    explicit BitArray(int num_bits, unsigned long value = 0);
+    BitArray(const BitArray& b);
+
+    void swap(BitArray& b);
+
+    BitArray& operator=(const BitArray& b);
+
+    void resize(int num_bits, bool value = false);
+
+    void clear();
+
+    void push_back(bool bit);
+
+    BitArray& operator&=(const BitArray& b);
+
+    BitArray& operator|=(const BitArray& b);
+
+    BitArray& operator^=(const BitArray& b);
+
+    BitArray& operator<<=(int n);
+
+    BitArray& operator>>=(int n);
+
+    BitArray operator<<(int n) const;
+
+    BitArray operator>>(int n) const;
+
+    BitArray& set(int n, bool val = true);
+
+    BitArray& set();
+
+    BitArray& reset(int n);
+
+    BitArray& reset();
+
+    bool any() const;
+
+    bool none() const;
+
+    BitArray operator~() const;
+
+    int count() const;
+
+    bool operator[](int i) const;
+
+    int size() const;
+
+    bool empty() const;
+
+    std::string to_string() const;
 };
+
+bool operator==(const BitArray & a, const BitArray & b);
+bool operator!=(const BitArray & a, const BitArray & b);
+
+BitArray operator&(const BitArray& b1, const BitArray& b2);
+BitArray operator|(const BitArray& b1, const BitArray& b2);
+BitArray operator^(const BitArray& b1, const BitArray& b2);
 
 
 #endif //TASK_1_BITARRAY_H
