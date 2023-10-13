8527285cbdf986d45b697abbbb889c105444da6a
Alexander <a.tyurin@g.nsu.ru>
2023-10-13 20:35:30 +0700

    initial

---
+++ task-1/BitArray.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Alexander on 13.10.2023.
+//
+
+#include "BitArray.h"
---
+++ task-1/BitArray.h
@@ -0,0 +1,14 @@
+//
+// Created by Alexander on 13.10.2023.
+//
+
+#ifndef TASK_1_BITARRAY_H
+#define TASK_1_BITARRAY_H
+
+
+class BitArray {
+
+};
+
+
+#endif //TASK_1_BITARRAY_H
