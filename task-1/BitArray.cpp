19552145a8a7f62357368f988cce00f675ac609d
Alexander <a.tyurin@g.nsu.ru>
2023-10-25 01:13:30 +0700

    added constructors

--- task-1/BitArray.cpp
+++ task-1/BitArray.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "BitArray.h"
+
+#define BITS_PER_BLOCK 64
+
+BitArray::BitArray() {
+    num_bits = 0;
+}
+
+BitArray::BitArray(int num_bits, unsigned long value) {
+    int new_size = static_cast<int>((num_bits - 1) / BITS_PER_BLOCK) + 1;
+    blocks.resize(new_size);
+
+    this->num_bits = num_bits;
+    blocks[0] = value;
+}
+
+BitArray::BitArray(const BitArray &b) {
+    std::copy(b.blocks.begin(), b.blocks.end(), blocks.begin());
+    num_bits = b.num_bits;
+}
+
